3188ce88da260e582374c34186331fe737714e1b
Reuben Victoria <reubenvickie20@gmail.com>
2022-12-23 08:35:06 +0100

    Fixes on the padding

--- src/Container/EditProfile/EditProfile.js
+++ src/Container/EditProfile/EditProfile.js
@@ -1,9 +1,7 @@
-import React from "react";
+import React from 'react';
 
-function EditProfile(){
-    return(
-        <div>
-            
-        </div>
-    )
+function EditProfile() {
+  return <div></div>;
 }
+
+export default EditProfile;
