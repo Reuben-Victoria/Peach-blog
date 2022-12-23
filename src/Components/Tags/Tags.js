782862ffd5f0ec7f1a88905152694305679e5141
Reuben Victoria <reubenvickie20@gmail.com>
2022-12-23 11:32:04 +0100

    Create Tag component

---
+++ src/Components/Tags/Tags.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Button from '../../Common/Button/Button';
+import { Link } from 'react-router-dom';
+import dummy from '../../assets/dummy.svg';
+import styles from './Tags.module.scss';
+import ProfileData from '../../Common/ProfileData/ProfileData';
+import close from '../../assets/close.svg';
+
+function Tags() {
+  return (
+    <div className={styles.publications}>
+      <h1>Top Stories</h1>
+      <div>
+        <ProfileData src={dummy} name={'Jankin Handins'} />
+        <h2>How to earn Money</h2>
+      </div>
+      <div>
+        <ProfileData src={dummy} name={'Jankin Handins'} />
+        <h2>How to earn Money</h2>
+      </div>
+      <div>
+        <ProfileData src={dummy} name={'Jankin Handins'} />
+        <h2>How to earn Money</h2>
+      </div>
+      <p className={styles.publications__list}>View full List</p>
+      <div className={styles.publications__faq}>
+        <div>
+          <h1>Writing on Peach</h1>
+          <p>New Writer FAQ</p>
+          <p>Writing advice</p>
+          <p>Grow your mentorship</p>
+          <Link to="/create-post">
+            <Button theme={'secondary'} size={'md'} text={'Start Writing'} />
+          </Link>
+        </div>
+        <div>
+          <img src={close} alt="close-icon" />
+        </div>
+      </div>
+      <div className={styles.publications__recommended}>
+        <h2>Recommended Topics</h2>
+        <div className={styles.publications__recommended__buttons}>
+          <Button theme={'primary'} size={'smaller'} text={'Programming'} />
+          <Button theme={'primary'} size={'smaller'} text={'Data Science'} />
+          <Button theme={'primary'} size={'smaller'} text={'Technology'} />
+        </div>
+        <div className={styles.publications__recommended__buttons}>
+          <Button theme={'primary'} size={'smaller'} text={'Self Improvement'} />
+          <Button theme={'primary'} size={'smaller'} text={'Writing'} />
+          <Button theme={'primary'} size={'smaller'} text={'Relationships'} />
+        </div>
+        <div className={styles.publications__recommended__buttons}>
+          <Button theme={'primary'} size={'smaller'} text={'Machine Learning'} />
+          <Button theme={'primary'} size={'smaller'} text={'Productivity'} />
+          <Button theme={'primary'} size={'smaller'} text={'Politics'} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Tags;
