05a492efbb309947356ddc3585ad1a2c68311049
Reuben Victoria <reubenvickie20@gmail.com>
2023-01-05 11:47:11 +0100

    Fixed width for Authentication screens

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,10 +3,32 @@ import { Link } from 'react-router-dom';
 import image from '../../assets/HeaderLogo.svg';
 // import RequireAuth from '../RequireAuth';
 import styles from './Header.module.scss';
+// const { userInfo } = useSelector((state) => state.auth);
 
 function Header() {
+  // const token = localStorage.getItem('userToken');
+
+  // const displayHeader = () => {
+  //   if (token !== null) {
+  //     return (
+  //       <Link to="/">
+  //         <div className={styles.logo}>
+  //           <img src={image} alt="Logo" />
+  //         </div>
+  //       </Link>
+  //     );
+  //   } else {
+  //     return (
+  //       <div className={styles.logo}>
+  //         <img src={image} alt="Logo" />
+  //       </div>
+  //     );
+  //   }
+  // };
+
   return (
     <header>
+      {' '}
       <Link to="/">
         <div className={styles.logo}>
           <img src={image} alt="Logo" />
