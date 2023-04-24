fd8e5f759789d442b76a7e94238b7f746eaba551
Reuben Victoria <reubenvickie20@gmail.com>
2023-04-24 08:04:22 +0100

    style: clean code

--- src/pages/EditPostPage.js
+++ src/pages/EditPostPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import EditPost from '../Container/EditPost/EditPost';
-import PageLayout from '../Layouts/PageLayout';
+import EditPost from 'container/editPost/EditPost';
+import PageLayout from 'layouts/PageLayout';
 
 function EditPostPage() {
   return (
