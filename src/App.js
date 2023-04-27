4b9b6b8c5d71eca02fb17e0b1362d70b3beb6547
Reuben Victoria <reubenvickie20@gmail.com>
2023-04-27 10:20:40 +0100

    chore: node modules set up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import './App.css';
-// import Success from './Components/Success/Success';
 import 'react-toastify/dist/ReactToastify.css';
 import Router from './router';
-// import RequireAuth from './Components/RequireAuth';
-// import Input from './Common/Input/Input';
 
 function App() {
   return (
